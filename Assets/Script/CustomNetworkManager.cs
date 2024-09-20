93bb4f7e683c4be186bfff3f5526117da42c4c8c
qualificationalitated <hk23g@fsu.edu>
2024-09-20 16:54:40 +0900

    [feat] 유니티 - 몽고DB 연결

--- Assets/Script/CustomNetworkManager.cs
+++ Assets/Script/CustomNetworkManager.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 public class CustomNetworkManager : MonoBehaviour
 {
     private NetworkManager networkManager;
     [SerializeField] GameObject PlayerPrefab;
+
+    // MongoDB 클라이언트 및 데이터베이스 변수
+    private IMongoClient mongoClient;
+    private IMongoDatabase database;
+
+    private string mongoDBUserName = "work2gatherAdmin";
+
+    private string mongoDBPassword = "BqaqH3zsAL3xSM1o";
+
+    private string mongoDBName = "work2gather";
 
     void Start()
     {
@@ -23,6 +35,7 @@ public class CustomNetworkManager : MonoBehaviour
     private void HandleServerStarted()
     {
         Debug.Log("서버가 시작되었습니다.");
+        ConnectToMongoDB(); // MongoDB 연결
     }
 
     // 클라이언트가 연결되었을 때 호출될 함수
@@ -58,5 +71,21 @@ public class CustomNetworkManager : MonoBehaviour
     public void StartClient()
     {
         networkManager.StartClient();
+    }
+
+    // MongoDB 연결 함수
+    // MongoDB 연결 함수
+    void ConnectToMongoDB()
+    {
+        // MongoDB 연결 문자열
+        string connectionString = $"mongodb+srv://{mongoDBUserName}:{mongoDBPassword}@qualificationalitated.rc7ev.mongodb.net/?retryWrites=true&w=majority&appName=qualificationalitated";
+
+        // MongoDB 클라이언트 생성
+        mongoClient = new MongoClient(connectionString);
+
+        // 데이터베이스 선택
+        database = mongoClient.GetDatabase(mongoDBName); // 데이터베이스 이름 변경 필요
+
+        Debug.Log("MongoDB에 연결되었습니다.");
     }
 }
